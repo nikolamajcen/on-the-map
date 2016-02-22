190c103b1fd3d8a397ebd102f82665b1a5e5aba0
Nikola Majcen <nikola.majcen@gmail.com>
2016-02-23 00:22:47 +0100

    Created view for adding location

---
+++ On The Map/FindLocationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FindLocationViewController.swift
+//  On The Map
+//
+//  Created by Nikola Majcen on 23/02/16.
+//  Copyright © 2016 Nikola Majcen. All rights reserved.
+//
+
+import UIKit
+
+class FindLocationViewController: UIViewController {
+    
+    @IBAction func cancel(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
