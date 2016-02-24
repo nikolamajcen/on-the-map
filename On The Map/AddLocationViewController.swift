520566f0f98384f6828913560bdc7d2a0bc6d68f
Nikola Majcen <nikola.majcen@gmail.com>
2016-02-24 22:20:24 +0100

    Added view for adding user location

---
+++ On The Map/AddLocationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddLocationViewController.swift
+//  On The Map
+//
+//  Created by Nikola Majcen on 24/02/16.
+//  Copyright © 2016 Nikola Majcen. All rights reserved.
+//
+
+import Foundation
